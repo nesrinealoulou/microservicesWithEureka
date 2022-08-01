1fcb3c4e336834d4fd0c837670a8145982086836
nessrine <Nessrine.aloulou@enis.tn>
2022-08-01 12:37:07 +0100

    first commit

--- gateway-service/src/main/java/com/beprimtech/managment/registryservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/beprimtech/managment/registryservice/GatewayServiceApplication.java
@@ -13,9 +13,10 @@ public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
+
     @Bean
-    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
-        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
 }
